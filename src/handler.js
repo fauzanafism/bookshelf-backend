e43e7d4c4c53cb86ee72bf7e75937d1cf8d90802
fauzanafism <fauzan137@gmail.com>
2022-01-29 05:34:24 +0700

    changed style guide from standard to airbnb

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,7 @@
 const addBookHandler = (request, h) => {
     const { name, year, author, summary, publisher, readPage, reading } = request.payload;
+
+    const id = nanoid(16);
 };
 
 module.exports = {addBookHandler};
