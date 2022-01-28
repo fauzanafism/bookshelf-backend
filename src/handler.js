d7552d11319356b4d4138585741b6680794d80eb
fauzanafism <fauzan137@gmail.com>
2022-01-29 05:43:30 +0700

    migrated from standard style guide to airbnb

--- src/handler.js
+++ src/handler.js
@@ -1,9 +1,11 @@
-const {nanoid} = require('nanoid');
+const { nanoid } = require('nanoid');
 
 const addBookHandler = (request, h) => {
-  const { name, year, author, summary, publisher, readPage, reading } = request.payload;
+  const {
+    name, year, author, summary, publisher, readPage, reading,
+  } = request.payload;
 
-    const id = nanoid(16);
+  const id = nanoid(16);
 };
 
-module.exports = {addBookHandler};
+module.exports = { addBookHandler };
